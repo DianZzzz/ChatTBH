1075b4820943e7f85ca44b6659ec9979c7d1bf7a
DianZhao <55634814+DianZzzz@users.noreply.github.com>
2023-02-09 22:24:51 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <meta property="og:description" content="Up your game on Tinder, Hinge and Bumble" key="description"/>
         <meta
           property="og:image"
-          content="https://github.com/DianZzzz/ChatTBH/blob/main/assets/image.png"
+          content="https://github.com/DianZzzz/ChatTBH/blob/main/assets/image.png?raw=true"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
